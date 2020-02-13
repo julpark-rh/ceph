c1562e846bb3000f42d18a7563b243355b3f2a82
julpark-rh <yonhyun@gmail.com>
2020-02-13 14:01:10 -0800

    create pool named julius
    Signed-off-by: julpark-rh <yonhyun@gmail.com>

--- qa/workunits/rbd/krbd_data_pool.sh
+++ qa/workunits/rbd/krbd_data_pool.sh
@@ -111,7 +111,8 @@ ceph osd pool create rbdnonzero 24 24
 rbd pool init rbdnonzero
 ceph osd pool create clonesonly 24 24
 rbd pool init clonesonly
-echo "how r u"
+ceph osd pool create julius 24 24
+rbd pool init julius
 for pool in rbd rbdnonzero; do
     rbd create --size 200 --image-format 1 $pool/img0
     rbd create --size 200 $pool/img1
